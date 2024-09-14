66b5db93a75c06a4f55e8364df87f9abd3ee7a5f
mehrdad kalateh <mehrdad1234kalateh@gmial.com>
2024-09-14 23:21:43 +0330

    authentication module completed

--- php/auth/auth.php
+++ php/auth/auth.php
@@ -1,24 +1,78 @@
 <?php
 use database\Database;
-    require_once "../database_modules/database.php";
-    class Auth extends Database{
-        private $table;
-        public function __construct(){
-            parent::__construct();
-            $this->table = 'users';
-        }
+require_once "../database_modules/database.php";
 
-        public function login(array $info_assoc){
-            try{
-                $user = $this->read($this->table);
-                print_r($user);
-            }catch(Exception $e){
-                echo $e;
+class Auth extends Database {
+    public $table;
+
+    public function __construct() {
+        parent::__construct();
+        $this->table = "users";
+    }
+
+    public function login(array $info_assoc) {
+        try {
+            $user = $this->read($this->table, ['username', 'password'], ['=', "="], [$info_assoc['username'], $info_assoc['password']]);
+            if(count($user) > 0) {
+                return false;
             }
+            return true;
+        } catch(Exception $e) {
+            echo $e;
+            return false; 
         }
     }
 
-    $auth = new Auth();
+    public function logout() {
+        
+    }
 
-    $auth->login(['username'=>"mehrdad", "password" => "2234"]);
+    public function register(array $insertinig_info) {
+        try {
+            $users = $this->create($this->table, ["username" => $insertinig_info['username'], "password" => $insertinig_info['password']]);
+            if($users) {
+                return true;
+            }
+            return false; 
+        } catch(Exception $e) {
+            echo $e;
+            return false;
+        }
+    }
 
+    public function deleteUser($username) {
+        try {
+            $result = $this->delete($this->table, ['username' => $username]);
+            if ($result) {
+                return true; 
+            }
+            return false; 
+        } catch(Exception $e) {
+            echo $e;
+            return false;
+        }
+    }
+}
+
+// $auth = new Auth();
+
+// // Uncomment to test the login
+// // $auth->login(['username' => "mehrdad", "password" => "1223"]);
+
+// // Test registration
+// $auth->register(['username' => "ali", "password" => "34565"]);
+
+// // Test logout
+// if ($auth->logout()) {
+//     echo "Logout successful!";
+// } else {
+//     echo "No user was logged in or logout failed.";
+// }
+
+// // Test deleting user
+// if ($auth->deleteUser("ali")) {
+//     echo "User deleted successfully!";
+// } else {
+//     echo "User deletion failed!";
+// }
+?>
